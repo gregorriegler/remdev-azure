50853ea2846d1ec3db8cc0208d15d3e0fe4e6c60
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-08 22:04:29 +0200

    wip

--- start.sh
+++ start.sh
@@ -1,20 +1,23 @@
 #!/bin/bash 
-
+echo "deleting group mob..."
 az group delete --name mob
 
+echo "recreating group mob..."
 az group create --name mob --location germanywestcentral
 
-# responds with "publicIpAddress": "51.116.184.244",
-ip = $(az vm create \
+echo "creating vm mobVm..."
+ip=$(az vm create \
 --resource-group mob \
 --name mobVm \
 --size Standard_D4s_v3 \
 --image Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest \
 --admin-username gregor \
 --generate-ssh-keys \
---custom-data cloud-init.yml | jq .publicIpAddress)
+--public-ip-sku Standard \
+--custom-data cloud-init.yml | jq -r .publicIpAddress)
 
 #anydesk ports
+echo "opening ports for anydesk (443, 6568, 7070)..."
 az vm open-port --port 443 --resource-group mob --name mobVm --priority 1100
 az vm open-port --port 6568 --resource-group mob --name mobVm --priority 1300
 az vm open-port --port 7070 --resource-group mob --name mobVm --priority 1400
@@ -26,4 +29,4 @@ az vm user update \
   --password gregor
 
 echo "vm created."
-echo "ip: $ip"
+echo "ssh ${ip}"
