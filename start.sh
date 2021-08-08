9e1abe89496fa4f501c474951110c004d6b5fb05
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-08 23:28:44 +0200

    running with vnc

--- start.sh
+++ start.sh
@@ -18,9 +18,9 @@ ip=$(az vm create \
 
 #anydesk ports
 echo "opening ports for anydesk (443, 6568, 7070)..."
-az vm open-port --port 443 --resource-group mob --name mobVm --priority 1100
-az vm open-port --port 6568 --resource-group mob --name mobVm --priority 1300
-az vm open-port --port 7070 --resource-group mob --name mobVm --priority 1400
+az vm open-port --port 443 --resource-group mob --name mobVm --priority 1100 &
+az vm open-port --port 6568 --resource-group mob --name mobVm --priority 1300 &
+az vm open-port --port 7070 --resource-group mob --name mobVm --priority 1400 &
 
 az vm user update \
   --resource-group mob \
@@ -29,4 +29,8 @@ az vm user update \
   --password gregor
 
 echo "vm created."
+echo "connect via ssh:"
 echo "ssh ${ip}"
+echo "or via vnc:"
+echo "ssh -L 5901:127.0.0.1:5901 -N -f -l gregor ${ip}"
+echo "xtigervncviewer -DesktopSize=1920x1080 -RemoteResize=0 :1"
