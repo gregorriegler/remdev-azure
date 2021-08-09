db3e9d15544abb801e32b9ca248eacda929c7dd5
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-09 22:17:16 +0200

    go

--- start.sh
+++ start.sh
@@ -18,9 +18,11 @@ ip=$(az vm create \
 
 #anydesk ports
 echo "opening ports for anydesk (443, 6568, 7070)..."
+az vm open-port --port 80 --resource-group mob --name mobVm --priority 1000 &
 az vm open-port --port 443 --resource-group mob --name mobVm --priority 1100 &
 az vm open-port --port 6568 --resource-group mob --name mobVm --priority 1300 &
 az vm open-port --port 7070 --resource-group mob --name mobVm --priority 1400 &
+az vm open-port --port 5938 --resource-group mob --name mobVm --priority 1500 &
 
 az vm user update \
   --resource-group mob \
