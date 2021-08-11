be0607751f939536162946948e20b73e0a594bff
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-12 00:29:32 +0200

    more tools

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
-echo "deleting group mob..."
+
+echo "deleting group mob ..."
 az group delete --name mob
 
-echo "recreating group mob..."
+echo "recreating group mob ..."
 az group create --name mob --location germanywestcentral
 
-echo "creating vm mobVm..."
+echo "creating vm mobVm ..."
 ip=$(az vm create \
 --resource-group mob \
 --name mobVm \
@@ -16,13 +17,12 @@ ip=$(az vm create \
 --public-ip-sku Standard \
 --custom-data cloud-init.yml | jq -r .publicIpAddress)
 
-#anydesk ports
-echo "opening ports for anydesk (443, 6568, 7070)..."
-az vm open-port --port 80 --resource-group mob --name mobVm --priority 1050
-az vm open-port --port 443 --resource-group mob --name mobVm --priority 1100
-az vm open-port --port 6568 --resource-group mob --name mobVm --priority 1300
-az vm open-port --port 7070 --resource-group mob --name mobVm --priority 1400
-az vm open-port --port 5938 --resource-group mob --name mobVm --priority 1500
+echo "opening ports ..."
+az vm open-port --port 80 --resource-group mob --name mobVm --priority 1050   # anydesk
+az vm open-port --port 443 --resource-group mob --name mobVm --priority 1100  # anydesk
+az vm open-port --port 6568 --resource-group mob --name mobVm --priority 1300 # anydesk
+az vm open-port --port 7070 --resource-group mob --name mobVm --priority 1400 # anydesk
+az vm open-port --port 5938 --resource-group mob --name mobVm --priority 1500 # teamviewer
 
 az vm user update \
   --resource-group mob \
@@ -33,6 +33,6 @@ az vm user update \
 echo "vm created."
 echo "connect via ssh:"
 echo "ssh ${ip}"
-echo "or via vnc:"
-echo "ssh -L 5901:127.0.0.1:5901 -N -f -l gregor ${ip}"
-echo "xtigervncviewer -DesktopSize=1920x1080 -RemoteResize=0 :1"
+
+# TODO
+# Password as an argument
