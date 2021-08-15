83d08e37b7051c23f2c00746be604b52921acb6e
Gregor Riegler <gregor.riegler@ebcont.com>
2021-08-15 14:41:37 +0200

    user:mob, password as argument, fix keystroke issues

--- start.sh
+++ start.sh
@@ -1,38 +1,36 @@
 #!/bin/bash 
 
+REGION='germanywestcentral'
+SIZE='Standard_D4s_v3' # Alternative: 'Standard_D8s_v4' // General Purpose vCPUs:8 (Intel® Xeon® Platinum 8272CL) memory: 32GiB
+USERNAME='mob'
+PASSWORD=${1:-mob}
+
 echo "deleting group mob ..."
 az group delete --name mob
 
 echo "recreating group mob ..."
-az group create --name mob --location germanywestcentral
+az group create --name mob --location $REGION
 
 echo "creating vm mobVm ..."
 ip=$(az vm create \
 --resource-group mob \
 --name mobVm \
---size Standard_D4s_v3 \
+--size $SIZE \
 --image Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest \
---admin-username gregor \
+--admin-username $USERNAME \
 --generate-ssh-keys \
 --public-ip-sku Standard \
 --custom-data cloud-init.yml | jq -r .publicIpAddress)
 
 echo "opening ports ..."
-az vm open-port --port 80 --resource-group mob --name mobVm --priority 1050   # anydesk
-az vm open-port --port 443 --resource-group mob --name mobVm --priority 1100  # anydesk
-az vm open-port --port 6568 --resource-group mob --name mobVm --priority 1300 # anydesk
-az vm open-port --port 7070 --resource-group mob --name mobVm --priority 1400 # anydesk
-az vm open-port --port 5938 --resource-group mob --name mobVm --priority 1500 # teamviewer
+az vm open-port --port 80,443,6568,7070,5938 --resource-group mob --name mobVm --priority 1050
 
 az vm user update \
   --resource-group mob \
   --name mobVm \
-  --username gregor \
-  --password gregor
+  --username $USERNAME \
+  --password $PASSWORD
 
 echo "vm created."
 echo "connect via ssh:"
-echo "ssh ${ip}"
-
-# TODO
-# Password as an argument
+echo "ssh mob@${ip}"
