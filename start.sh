acfed15032c82ab11e01c8b4a925896c1f5bc741
Gregor Riegler <gregor.riegler@ebcont.com>
2022-01-24 13:21:43 +0100

    parameterize start script

--- start.sh
+++ start.sh
@@ -1,22 +1,33 @@
 #!/bin/bash 
-
 REGION='germanywestcentral'
 # Alternative: 'Standard_D8s_v4' // General Purpose vCPUs:8 (Intel® Xeon® Platinum 8272CL) memory: 32GiB
 # Standard_D4d_v4
 # Standard_D4s_v3
-SIZE='Standard_D4d_v4'
+SIZE='Standard_D8s_v4'
+GROUPNAME='mob'
 USERNAME='mob'
-PASSWORD=${1:-mob}
+PASSWORD='mob'
 
-echo "deleting group mob ..."
-az group delete --name mob
+while getopts r:s:g:u:p flag
+do
+    case "${flag}" in
+        r) REGION=${OPTARG};;
+        s) SIZE=${OPTARG};;
+        g) GROUPNAME=${OPTARG};;
+        u) USERNAME=${OPTARG};;
+        p) PASSWORD=${OPTARG};;
+    esac
+done
 
-echo "recreating group mob ..."
-az group create --name mob --location $REGION
+echo "deleting group $GROUPNAME ..."
+az group delete --name $GROUPNAME
+
+echo "recreating group $GROUPNAME ..."
+az group create --name $GROUPNAME --location $REGION
 
 echo "creating vm mobVm ..."
 ip=$(az vm create \
---resource-group mob \
+--resource-group $GROUPNAME \
 --name mobVm \
 --size $SIZE \
 --image Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest \
@@ -26,15 +37,15 @@ ip=$(az vm create \
 --custom-data cloud-init.yml | jq -r .publicIpAddress)
 
 echo "opening ports ..."
-az vm open-port --port 80   --resource-group mob --name mobVm --priority 1050 > /dev/null
-az vm open-port --port 443  --resource-group mob --name mobVm --priority 1060 > /dev/null
-az vm open-port --port 3000 --resource-group mob --name mobVm --priority 1070 > /dev/null
-az vm open-port --port 5938 --resource-group mob --name mobVm --priority 1080 > /dev/null
-az vm open-port --port 6568 --resource-group mob --name mobVm --priority 1090 > /dev/null
-az vm open-port --port 7070 --resource-group mob --name mobVm --priority 1100 > /dev/null
+az vm open-port --port 80   --resource-group $GROUPNAME --name mobVm --priority 1050 > /dev/null
+az vm open-port --port 443  --resource-group $GROUPNAME --name mobVm --priority 1060 > /dev/null
+az vm open-port --port 3000 --resource-group $GROUPNAME --name mobVm --priority 1070 > /dev/null
+az vm open-port --port 5938 --resource-group $GROUPNAME --name mobVm --priority 1080 > /dev/null
+az vm open-port --port 6568 --resource-group $GROUPNAME --name mobVm --priority 1090 > /dev/null
+az vm open-port --port 7070 --resource-group $GROUPNAME --name mobVm --priority 1100 > /dev/null
 
 az vm user update \
-  --resource-group mob \
+  --resource-group $GROUPNAME \
   --name mobVm \
   --username $USERNAME \
   --password $PASSWORD > /dev/null
